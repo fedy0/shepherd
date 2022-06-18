82684147de4032f895c881095726288b7dd46686
fedy0 <ifedioraelochukwuc@gmail.com>
2022-06-19 00:09:49 +0200

    remove test scripts

--- software/firmware/tools/pru-elf-2022.05.amd64/lib/gcc/pru-elf/12.1.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/mnt/hdd/dinux/misc/pru/.build/pru-elf/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=pru-elf --prefix=/home/dinux/x-tools/pru-elf --exec_prefix=/home/dinux/x-tools/pru-elf --with-local-prefix=/home/dinux/x-tools/pru-elf/pru-elf --with-headers=/home/dinux/x-tools/pru-elf/pru-elf/include --with-newlib --enable-threads=no --disable-shared --with-pkgversion='crosstool-NG 1.25.0.29_3dc2ff1' --enable-newlib-nano-formatted-io --enable-__cxa_atexit --disable-libgomp --disable-libmudflap --disable-libmpx --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libstdcxx-verbose --with-gmp=/mnt/hdd/dinux/misc/pru/.build/pru-elf/buildtools --with-mpfr=/mnt/hdd/dinux/misc/pru/.build/pru-elf/buildtools --with-mpc=/mnt/hdd/dinux/misc/pru/.build/pru-elf/buildtools --with-isl=/mnt/hdd/dinux/misc/pru/.build/pru-elf/buildtools --enable-lto --enable-target-optspace --disable-nls --disable-multilib --enable-languages=c,c++";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { NULL, NULL} };
