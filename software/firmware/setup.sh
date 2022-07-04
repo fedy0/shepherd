8fa0bdfd55b1312cc39079a6e6a94f046a80a6e7
Ifediora Elochukwu C <ifedioraelochukwuc@gmail.com>
2022-07-05 01:54:03 +0200

    Update setup.sh

--- software/firmware/setup.sh
+++ software/firmware/setup.sh
@@ -30,6 +30,7 @@ echo "#PRU GCC  supports" >> ~/.bashrc
 echo "export PRU_GCC=$PRU_GCC_BIN" >> ~/.bashrc
 echo "export PRU_SUPPORT=$PWD/pru-software-support-package" >> ~/.bashrc
 echo 'export PATH=$PATH:$PRU_GCC' >> ~/.bashrc
+export PS1=$PS1:pru
 source ~/.bashrc
 
 # 5. Patch PRU software support packages
