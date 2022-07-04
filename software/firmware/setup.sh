6d7f5d686d627426bc9470afabe5fe2e46d95591
Ifediora Elochukwu C <ifedioraelochukwuc@gmail.com>
2022-07-05 01:12:23 +0200

    Update setup.sh

--- software/firmware/setup.sh
+++ software/firmware/setup.sh
@@ -2,6 +2,7 @@
 # chmod +x setup.sh
 # sudo ./setup.sh pru_iep.patch
 
+TOOLCHAIN=pru-elf-2022.05.amd64
 TOOLS_DIR=tools
 
 # 1. Create a directory to host the tools
@@ -12,7 +13,7 @@ cd $TOOLS_DIR
 
 # 2. Install the cross-compiler toolchain by downloading and untar gnupru release
 echo "3. Downloading the cross-compiler toolchain..."
-wget -r -tries=2 https://github.com/dinuxbg/gnupru/releases/download/2022.05/pru-elf-2022.05.amd64.tar.xz --output-document=gcc-port -o log 
+wget -r -tries=2 https://github.com/dinuxbg/gnupru/releases/download/2022.05/$TOOLCHAIN.tar.xz --output-document=gcc-port -o log 
 echo "4. Untar'ing the cross-compiler toolchain..."
 tar -xf gcc-port
 echo "5. Deleting toolchain archive..."
@@ -24,7 +25,7 @@ git clone -b linux-4.19-rproc https://github.com/dinuxbg/pru-software-support-pa
 
 # 4. Add PRU gcc and binutils to your PATH
 echo "7. Adding PRU GCC supports to path..."
-export PRU_GCC_BIN=$PWD/pru-elf*/bin
+export PRU_GCC_BIN=$PWD/$TOOLCHAIN/bin
 echo "#PRU GCC  supports" >> ~/.bashrc
 echo "export PRU_GCC=$PRU_GCC_BIN" >> ~/.bashrc
 echo "export PRU_SUPPORT=$PWD/pru-software-support-package" >> ~/.bashrc
